decec8e4cb1a1afd8148a234db6bd84d37c7e44b
vkolesnik017 <52042636+vkolesnik017@users.noreply.github.com>
2026-03-11 11:16:26 +0200

    update BaseTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -11,7 +11,6 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public abstract class BaseTest {
     @BeforeMethod
     public void setup() {
-        //
         Configuration.browser = "chrome";
         Configuration.headless = false;
         Configuration.timeout = 10_000;
